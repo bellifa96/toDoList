31bf820b7c270c1b6c702d1d0168167e9fac6bad
bellifa96 <bellifa96@live.fr>
2021-05-12 10:56:29 +0200

    trash button and checkbox

--- App.js
+++ App.js
@@ -4,10 +4,13 @@ import {createStackNavigator} from '@react-navigation/stack'
 import HomeScreen from "./Views/HomeScreen"
 import {StatusBar} from "expo-status-bar";
 import ListScreen from "./Views/ListScreen";
+import { LogBox } from 'react-native';
 
 const Stack = createStackNavigator()
 
 const App = () => {
+    LogBox.ignoreLogs(['Setting a timer']);
+
     return (
         <>
             <StatusBar style="auto"/>
--- Views/ListScreen.js
+++ Views/ListScreen.js
@@ -2,9 +2,33 @@ import React from 'react'
 import {View, Text} from "react-native";
 import AddToDo from "../Components/AddToDo";
 import {styles} from "../assets/Styles";
+import BouncyCheckbox from "react-native-bouncy-checkbox";
+import Fire from "../Fire";
+import {Button} from "react-native-paper";
 
 const ListScreen = ({route}) => {
     const {list} = route.params
+
+    const fire =  new Fire(async error => {
+        if (error) return alert('Une erreur est survenue')
+        await firebase.updateList(list)
+        return function unsubscribe() {
+            firebase.detach()
+        }
+    })
+
+    const update = (key,isChecked)=>{
+          list.todos[key].completed = isChecked;
+          const up = () =>{ fire() }
+
+    }
+    const supprimer = (key)=>{
+        if (key > -1) {
+            list.todos.splice(key, 1);
+            console.log(list.todos)
+            const up = () =>{ fire() }
+        }
+    }
     return (
         <View style={styles.container}>
             <View style={styles.content}>
@@ -12,8 +36,21 @@ const ListScreen = ({route}) => {
                 <View>
                     {
                         list.todos.map((item, key) => (
-                            <View key={key}>
-                                <Text> {item.name}</Text>
+                            <View key={key} style={{margin:10,flexDirection:'row'}}>
+                                <BouncyCheckbox
+                                    size={25}
+                                    fillColor="red"
+                                    unfillColor="#FFFFFF"
+                                    text={item.name}
+                                    iconStyle={{ borderColor: "red" }}
+                                    isChecked = {item.completed}
+                                    onPress={(isChecked) => {update(key,isChecked)}}
+                                />
+                                <Button icon="trash-can-outline"
+                                        style={{ alignSelf:'flex-end',width:25 }}
+                                        labelStyle={{ fontSize: 25 }}
+                                        onPress={()=>supprimer(key)}
+                                />
                             </View>
                         ))
                     }
